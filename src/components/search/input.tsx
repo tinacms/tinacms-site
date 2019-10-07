164786b57782f7c7017840defc75ef117293c33e
jamespohalloran <james.p.ohalloran@gmail.com>
2019-10-07 11:59:28 -0300

    debounce search

--- src/components/search/input.tsx
+++ src/components/search/input.tsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import { connectSearchBox } from 'react-instantsearch-dom'
-
+import * as debounce from 'lodash/debounce'
 import { SearchIcon, Form, Input } from './styles'
 import styled from 'styled-components'
 
-export default connectSearchBox(({ refine, ...rest }) => (
-  <Form>
-    <Input type="text" placeholder="Search" aria-label="Search" onChange={e => refine(e.target.value)} {...rest} />
-    <SearchIcon>
-      <MagnifyingIconPlaceholder />
-    </SearchIcon>
-  </Form>
-)) as any
+export default connectSearchBox(({ refine, ...rest }) => {
+  const debouncedSearch = debounce((e: any) => refine(e.target.value), 300)
+  const onChange = (e: any) => {
+    e.persist()
+    debouncedSearch(e)
+  }
+
+  return (
+    <Form>
+      <Input type="text" placeholder="Search" aria-label="Search" onChange={onChange} {...rest} />
+      <SearchIcon>
+        <MagnifyingIconPlaceholder />
+      </SearchIcon>
+    </Form>
+  )
+}) as any
 
 //TODO - use a magnifying glass icon instead of a llama
 const MagnifyingIconPlaceholder = styled.div`
