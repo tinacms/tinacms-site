5e887354cbc1c117239b688dc4d95bd5a6ace31f
Scott Byrne <scott.byrne@forestry.io>
2019-12-03 17:03:40 -0400

    fix broken font issue

--- src/components/foundations/typography/components/Heading.tsx
+++ src/components/foundations/typography/components/Heading.tsx
@@ -1,49 +1,44 @@
-import * as React from 'react';
+import * as React from 'react'
 
-import { styledWrapper as styled } from 'utils/primitives';
-import { HeadingSizes } from 'components/Theme';
+import { styledWrapper as styled } from 'utils/primitives'
+import { HeadingSizes } from 'components/Theme'
 
-import { determineFontDimensions } from '../utils';
-import { Typography, TypographyProps } from './Typography';
+import { determineFontDimensions } from '../utils'
+import { Typography, TypographyProps } from './Typography'
 
 export interface HeadingProps extends TypographyProps {
   /** Additional CSS classes to add to the component. */
-  className?: string;
+  className?: string
   /** Additional CSS properties to add to the component. */
-  style?: React.CSSProperties;
+  style?: React.CSSProperties
   /** What HTML element to render the text as. */
-  as?: keyof JSX.IntrinsicElements | React.ComponentType<any>;
+  as?: keyof JSX.IntrinsicElements | React.ComponentType<any>
   /** Size value of the heading. */
-  size?: keyof HeadingSizes;
+  size?: keyof HeadingSizes
 }
 
 /**
  * This is a base `Text` element to handle typography elements.
  */
 const StyledText = styled(Typography)<HeadingProps>`
-  font-family: 'tuner-regular';
-`;
+  font-family: 'tuner-regular', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell',
+    'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+`
 
 /**
  * Heading component provided as a styled component primitive.
  */
 export const Heading: React.SFC<HeadingProps> = ({ children, as, size, color, ...rest }) => (
-  <StyledText
-    as={as}
-    color={color}
-    {...determineFontDimensions('heading', size)}
-    size={size}
-    {...rest}
-  >
+  <StyledText as={as} color={color} {...determineFontDimensions('heading', size)} size={size} {...rest}>
     {children}
   </StyledText>
-);
+)
 
 Heading.defaultProps = {
   as: 'h2',
   color: 'inherit',
   size: 800,
-  margin: 0
-};
+  margin: 0,
+}
 
-Heading.displayName = 'Heading';
+Heading.displayName = 'Heading'
