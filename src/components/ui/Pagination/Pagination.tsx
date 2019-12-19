538acaed13a56579855ac7b6102b9d9390c903c8
Kendall Strautman <kendallstrautman@gmail.com>
2019-12-19 11:32:00 -0800

    cleanup: removes unused imports

--- src/components/ui/Pagination/Pagination.tsx
+++ src/components/ui/Pagination/Pagination.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
-import { TocItem } from 'interfaces/nodes'
+
 import { Text, Heading } from 'components/foundations'
 import { space, breakpoints, colors } from 'utils/variables'
 
