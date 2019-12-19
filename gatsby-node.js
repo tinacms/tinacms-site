3bae9d1f16fcd2deca70c79c3992c1d60ea5b087
Kendall Strautman <kendallstrautman@gmail.com>
2019-12-19 12:14:13 -0800

    other: removes unused import

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,6 @@
 const fs = require('fs')
 const path = require('path')
 const { GraphQLBoolean } = require('gatsby/graphql')
-const { createFilePath } = require("gatsby-source-filesystem")
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
